46d7e9acd021527b9e7a01669d301e01817bea76
gregorygetchell <ggetchell@gmail.com>
2020-08-28 09:18:15 -0400

    First Initial Commit

---
+++ public/css/missing_num.js
@@ -0,0 +1,10 @@
+array = [1, 2, 3, 4, 5, 7, 8, 9, 10]
+function missing_num(array) {
+    for (i = 1; i <= 10; i++) {
+        if (!array.include(i)) {
+            console.log(missing_num);
+        }
+        
+    }
+    
+}
